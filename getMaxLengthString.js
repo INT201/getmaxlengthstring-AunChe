507e1a6ecb23132132557e9707ec287849aa638a
AunChe <aunchalee.test@mail.kmutt.ac.th>
2022-09-14 21:06:29 +0700

    test

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,26 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
-}
+  let longest = 0;
+  let longestString = []
+  if(arrayOfString === undefined || arrayOfString === null ){
+    return undefined;
+  }
+  for (let i = 0; i < arrayOfString.length; i++){
+    if(arrayOfString[i].length > length) {
+      longest = arrayOfString[i].length; 
+     }
+    }
+    for (let i = 0; i < arrayOfString.length; i++){
+      if(arrayOfString[i].length === length) {
+        longestString.push(arrayOfString[i]);
+      }
+    }  
+    return longestString;
+  }
+
+
+
+
+
 module.exports = getMaxLengthString
